50439ceaa9f7b340951b25c3429a668faf36da23
Andrianto Dwi Laksono <andrianto.dl@gmail.com>
2021-12-05 00:04:15 +0800

    use react-window for CitySelector

--- xtramile/ClientApp/src/components/CitySelector/CitySelector.tsx
+++ xtramile/ClientApp/src/components/CitySelector/CitySelector.tsx
@@ -1,12 +1,105 @@
-import { Autocomplete, TextField, CircularProgress, AutocompleteChangeDetails, AutocompleteChangeReason } from "@mui/material";
+import { Autocomplete, TextField, CircularProgress, AutocompleteChangeDetails, AutocompleteChangeReason, Typography, useTheme, useMediaQuery } from "@mui/material";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { locationAPIUrl } from "../../shared/links";
+import { ListChildComponentProps, VariableSizeList } from 'react-window';
 
 interface CitySelectorProps {
   countryName?: string;
   onChange: (city?: string) => void;
 }
+
+const LISTBOX_PADDING = 8;
+
+const renderRow = (props: ListChildComponentProps) => {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: (style.top as number) + LISTBOX_PADDING,
+  };
+
+  return (
+    <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
+      {dataSet[1]}
+    </Typography>
+  );
+};
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+const useResetCache = (data: any) => {
+  const ref = React.useRef<VariableSizeList>(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+const ListboxComponent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLElement>
+>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData: React.ReactChild[] = [];
+  (children as React.ReactChild[]).forEach(
+    (item: React.ReactChild & { children?: React.ReactChild[] }) => {
+      itemData.push(item);
+      itemData.push(...(item.children || []));
+    },
+  );
+
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), {
+    noSsr: true,
+  });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child: React.ReactChild) => {
+    if (child.hasOwnProperty('group')) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
 
 export const CitySelector = (props: CitySelectorProps) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -55,6 +148,8 @@ export const CitySelector = (props: CitySelectorProps) => {
   return (
     <Autocomplete
       id="countries"
+      disableListWrap
+      ListboxComponent={ListboxComponent}
       options={cities}
       isOptionEqualToValue={(option, value) => option === value}
       getOptionLabel={(option) => option}
@@ -78,6 +173,7 @@ export const CitySelector = (props: CitySelectorProps) => {
           }}
         />
       )}
+      renderOption={(props, option) => [props, option]}
       disabled={isLoading || !props.countryName}
       fullWidth
     />
--- xtramile/ClientApp/src/components/WeatherInfo/WeatherInfo.tsx
+++ xtramile/ClientApp/src/components/WeatherInfo/WeatherInfo.tsx
@@ -53,92 +53,95 @@ export const WeatherInfo = (props: WeatherInfoProps) => {
 
   return (
     <Grid container spacing={1} direction="column">
-      {
-        isLoading &&
+      {isLoading && (
         <Grid item xs={12}>
           <CircularProgress />
         </Grid>
-      }
-      <Grid item xs={12}>
-        <img
-          src={`https://openweathermap.org/img/w/${info?.current.weather[0].icon}.png`}
-          alt="weather-icon"
-        />
-        <Typography variant="body1">{props.cityName}</Typography>
-        <Typography variant="body1">
-          {`(${info?.current.weather[0].main})`}
-        </Typography>
-        <Typography variant="subtitle1">
-          {`${new Date((info?.current.dt || 0) * 1000).toLocaleString()}`}
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell rowSpan={2}>Location</TableCell>
-              <TableCell>Latitude</TableCell>
-              <TableCell>{info?.lat}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Longitude</TableCell>
-              <TableCell>{info?.lon}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell rowSpan={3}>Wind</TableCell>
-              <TableCell>Speed</TableCell>
-              <TableCell>{`${info?.current.wind_speed} miles/hour`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Degrees</TableCell>
-              <TableCell>{info?.current.wind_deg}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Gust</TableCell>
-              <TableCell>{`${info?.current.wind_gust} miles/hour`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Visibility</TableCell>
-              <TableCell
-                colSpan={2}
-              >{`${info?.current.visibility.toLocaleString()} metres`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Sky</TableCell>
-              <TableCell
-                colSpan={2}
-              >{`${info?.current.clouds.toLocaleString()} % clouds`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell rowSpan={2}>Temperature</TableCell>
-              <TableCell>Celcius</TableCell>
-              <TableCell>{`${info?.current.temp_celcius.toLocaleString()} degrees`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Fahrenheit</TableCell>
-              <TableCell>{`${info?.current.temp.toLocaleString()} degrees`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Dew Point</TableCell>
-              <TableCell
-                colSpan={2}
-              >{`${info?.current.dew_point.toLocaleString()} degrees Fahrenheit`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Relative Humidity</TableCell>
-              <TableCell
-                colSpan={2}
-              >{`${info?.current.humidity.toLocaleString()} %`}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Pressure</TableCell>
-              <TableCell
-                colSpan={2}
-              >{`${info?.current.pressure.toLocaleString()} hPa`}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Grid>
+      )}
+      {!isLoading && info && (
+        <>
+          <Grid item xs={12}>
+            <img
+              src={`https://openweathermap.org/img/w/${info?.current.weather[0].icon}.png`}
+              alt="weather-icon"
+            />
+            <Typography variant="body1">{props.cityName}</Typography>
+            <Typography variant="body1">
+              {`(${info?.current.weather[0].main})`}
+            </Typography>
+            <Typography variant="subtitle1">
+              {`${new Date((info?.current.dt || 0) * 1000).toLocaleString()}`}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell rowSpan={2}>Location</TableCell>
+                  <TableCell>Latitude</TableCell>
+                  <TableCell>{info?.lat}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Longitude</TableCell>
+                  <TableCell>{info?.lon}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell rowSpan={3}>Wind</TableCell>
+                  <TableCell>Speed</TableCell>
+                  <TableCell>{`${info?.current.wind_speed} miles/hour`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Degrees</TableCell>
+                  <TableCell>{info?.current.wind_deg}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Gust</TableCell>
+                  <TableCell>{`${info?.current.wind_gust} miles/hour`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Visibility</TableCell>
+                  <TableCell
+                    colSpan={2}
+                  >{`${info?.current.visibility.toLocaleString()} metres`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Sky</TableCell>
+                  <TableCell
+                    colSpan={2}
+                  >{`${info?.current.clouds.toLocaleString()} % clouds`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell rowSpan={2}>Temperature</TableCell>
+                  <TableCell>Celcius</TableCell>
+                  <TableCell>{`${info?.current.temp_celcius.toLocaleString()} degrees`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Fahrenheit</TableCell>
+                  <TableCell>{`${info?.current.temp.toLocaleString()} degrees`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Dew Point</TableCell>
+                  <TableCell
+                    colSpan={2}
+                  >{`${info?.current.dew_point.toLocaleString()} degrees Fahrenheit`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Relative Humidity</TableCell>
+                  <TableCell
+                    colSpan={2}
+                  >{`${info?.current.humidity.toLocaleString()} %`}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Pressure</TableCell>
+                  <TableCell
+                    colSpan={2}
+                  >{`${info?.current.pressure.toLocaleString()} hPa`}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 };
